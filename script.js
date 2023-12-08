06bfc19a1cd1a23f9d619fd0f4ffc5cb5765bd97
Darion Valdez <Darionvldz@gmail.com>
2023-12-08 21:42:45 +0100

    add hover functionality and animations to buttons

--- script.js
+++ script.js
@@ -3,15 +3,63 @@ const choices = ['rock', 'paper', 'scissors'];
 let playerScore = 0;
 let computerScore = 0;
 
-let player = 'test';
-let computer = 'test';
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('#paper-button');
+const scissorsButton = document.querySelector('#scissors-button');
+
+let isPlaying = false;
+
+document.addEventListener('click',(event)=> {
+    console.log(event.target.id);
+    switch (event.target.id) {
+        case 'rock-button':
+            playRound('rock');
+        break;
+
+        case 'paper-button':
+            playRound('paper');
+        break;
+
+        case 'scissors-button':
+            playRound('scissors');
+        break;
+    }
+});
+
+//Work around for hover state that is not working properly. When clicking a button the image is being translated upwards,
+//however the hover property prevented the image from being translated until the user removed the mouse pointer from the image.
+
+rockButton.addEventListener('mouseenter',()=> {
+    rockButton.classList.toggle('hoverState');
+});
+
+rockButton.addEventListener('mouseleave',()=> {
+    rockButton.classList.toggle('hoverState');
+});
+paperButton.addEventListener('mouseenter',()=> {
+    paperButton.classList.toggle('hoverState');
+});
+
+paperButton.addEventListener('mouseleave',()=> {
+    paperButton.classList.toggle('hoverState');
+});
+scissorsButton.addEventListener('mouseenter',()=> {
+    scissorsButton.classList.toggle('hoverState');
+});
+
+scissorsButton.addEventListener('mouseleave',()=> {
+    scissorsButton.classList.toggle('hoverState');
+});
+
+
+
 
 function getComputerChoice(){
     return choices[getRandomInt(3)];
 }
 
 function logResult(){
-    console.log (`You picked ${player}, the computer picked ${computer}`);
+    
 }
 
 function logScore() {
@@ -46,14 +94,34 @@ function defeat(){
     console.log('DEAFEAT! You lost best of 5 games.');
 }
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection){
 
-    if (playerSelection == null) return;
+    switch (playerSelection){
+        case 'rock':
+            rockButton.classList.remove('hoverState');
+            rockButton.classList.toggle('selected');
+            paperButton.classList.toggle('deselected');
+            scissorsButton.classList.toggle('deselected');
+        break;
 
-    playerSelection = playerSelection.toLowerCase();
+        case 'paper':
+            paperButton.classList.remove('hoverState');
+            paperButton.classList.toggle('selected');
+            rockButton.classList.toggle('deselected');
+            scissorsButton.classList.toggle('deselected');
+        break;
 
-    player = playerSelection.toLowerCase();
-    computer = computerSelection;
+        case 'scissors':
+            scissorsButton.classList.remove('hoverState');
+            scissorsButton.classList.toggle('selected');
+            paperButton.classList.toggle('deselected');
+            rockButton.classList.toggle('deselected');
+        break;
+    }
+    
+    
+
+    computerSelection = getComputerChoice();
 
         switch (computerSelection) {
             case 'rock':
@@ -96,4 +164,4 @@ function game () {
     }
 }
 
-game();
+
